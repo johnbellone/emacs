4bbf86c38abba40c4637af349548626445c08603
John Bellone <jb@thunkbrightly.com>
2010-06-14 20:58:49 -0400

    Initial commit

---
+++ jb/defaults.el
@@ -0,0 +1,17 @@
+(setq-default inhibit-default-init t)
+(setq-default inhibit-startup-message t)
+(setq-default grep-command "find . | xargs grep -n -i ")
+(setq-default x-select-enable-clipboard t)
+(setq-default transient-mark-mode t)
+(setq-default indent-tabs-mode nil)
+(setq-default auto-save-default nil)
+(setq-default make-backup-files nil)
+
+(prefer-coding-system 'utf-8)
+
+(column-number-mode t)
+(blink-cursor-mode 0)
+(tool-bar-mode 0)
+(show-paren-mode t)
+(mouse-wheel-mode t)
+(scroll-bar-mode 0)
