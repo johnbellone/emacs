0d8165f2a080ebb9675ab77d0568eab308c77bd6
John Bellone <jb@thunkbrightly.com>
2010-07-02 13:36:53 -0400

    Removed duplicate loading of local.el

--- jb.el
+++ jb.el
@@ -1,9 +1,5 @@
 (add-to-list 'load-path "~/.emacs.d/vendor")
 
-; Custom changes that are usually per machine.
-(when (file-exists-p "local")
-  (load "local"))
-
 (load "jb/defaults")
 (load "jb/functions")
 
