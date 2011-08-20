f5729a05efb1a9ec9786dd274c8cbf8c8e08f3a6
John Bellone <jb@thunkbrightly.com>
2011-08-20 11:26:43 -0400

    Removing some test stuff.

--- jb/php.el
+++ jb/php.el
@@ -1,7 +1,5 @@
 ;; Extensions
-(add-to-list 'auto-mode-alist '(("\\.php" . html-php-mode))
+(add-to-list 'auto-mode-alist '(("\\.php" . html-php-mode)))
 
-(defun spark (task)
-  (interactive (list (completing-read "Sparks (default: help): "
-                                      (pcmpl-rake-tasks))))
-  (shell-command-to-string (concat "tools/spark " (if (= 0 (length task)) "default" task))))
+
+
