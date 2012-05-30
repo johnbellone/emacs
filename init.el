c4505a0da1dd81ecc80d3ac306a5d27c26f34ae1
John Bellone <jb@thunkbrightly.com>
2012-05-30 19:51:12 -0400

    Removing elpa.

--- init.el
+++ init.el
@@ -1,10 +1,5 @@
 (add-to-list 'load-path "~/.emacs.d")
 
-(when
-    (load
-     (expand-file-name "~/.emacs.d/elpa/package.el"))
-  (package-initialize))
-
 (load "jb")
 
 (ignore-errors (load "local"))
