b250b907e872ce32782575553fb0b4cff74836c4
John Bellone <jb@thunkbrightly.com>
2012-05-30 20:18:08 -0400

    There is no need for this package loader anymore. It comes by default in emacs24

--- init.el
+++ init.el
@@ -1,15 +1,5 @@
 (add-to-list 'load-path "~/.emacs.d")
 
-;;; This was installed by package-install.el.
-;;; This provides support for the package system and
-;;; interfacing with ELPA, the package archive.
-;;; Move this code earlier if you want to reference
-;;; packages in your .emacs.
-(when
-    (load
-     (expand-file-name "~/.emacs.d/elpa/package.el"))
-  (package-initialize))
-
 (load "jb")
 
 (ignore-errors (load "local"))
