b88a0f6e48834944dd98b5d01696f6a885569895
John Bellone <jb@thunkbrightly.com>
2012-06-03 22:42:48 -0400

    Adding new themes.

--- themes.el
+++ themes.el
@@ -2,10 +2,10 @@
   (add-to-list 'custom-theme-load-path themes)
   (dolist (f (directory-files themes))
     (let ((name (concat themes "/" f)))
-      (when (and file-directory-p name)
-        (not (equal f ".."))
-        (not (equal f "."))
-        (not (equal f ".git")))
-      (add-to-list 'custom-theme-load-path name))))
+      (when (and (file-directory-p name)
+                 (not (equal f ".."))
+                 (not (equal f ".")))
+        (add-to-list 'custom-theme-load-path name)))))
+
 
 
