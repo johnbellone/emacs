b8568a7c168784d313a5b4197f84243ddefba72d
John Bellone <jb@thunkbrightly.com>
2012-08-09 22:50:27 -0400

    Changing some global.el settings into session.el.
    For the most part I got rid of some of the older Emacs 21 shit
    that has been lying around for about four years now. I'm solely
    running Emacs 24 everywhere.

---
+++ session.el
@@ -0,0 +1,20 @@
+;; These are frame settings which affect a whole 'window' in Emacs.
+(setq-default inhibit-default-init t)
+(setq-default inhibit-default-init t)
+(setq-default inhibit-startup-message t)
+
+(setq-default indent-tabs-mode nil)
+(setq-default auto-save-default nil)
+(setq-default make-backup-files nil)
+
+(setq-default tramp-default-method "scp")
+
+(cua-mode 0)
+(show-paren-mode t)
+(mouse-wheel-mode t)
+(scroll-bar-mode 0)
+(prefer-coding-system 'utf-8)
+
+(tool-bar-mode -1)
+
+(global-font-lock-mode t)
