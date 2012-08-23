03ffb664700ebcb3ad65c963ab862552ffa842bb
John Bellone <jb@thunkbrightly.com>
2012-08-22 22:47:01 -0400

    Making some changes to spacing for c++.
    I got tired of the damn namespacing indenting. This also fixes
    opening braces indenting as well. For the most part though I
    follow Google C++ standard emacs file which is included.

--- c++/c++-mode.el
+++ c++/c++-mode.el
@@ -1,3 +1,15 @@
 ;; Just because I am too damn lazy to write my own.
 (add-hook 'c-mode-common-hook 'google-set-c-style)
 (add-hook 'c-mode-common-hook 'google-make-newline-indent)
+
+(c-add-style "custom-coding-style"
+             '((c-comment-only-line-offset . 0)
+               (c-hanging-braces-alist . ((substatement-open before after)))
+               (c-offsets-alist . ((innamespace . 0)
+                                   (substatement-open . 0)))))
+
+(add-hook 'c-mode-common-hook
+          '(lambda()
+             (c-set-style "custom-coding-style")
+             (setq tab-width 4)
+             (setq c-basic-offset tab-width)))  
