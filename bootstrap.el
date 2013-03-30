eb26fe107eec7e4e9f8a9da9facb487a1144d1c3
John Bellone <jb@thunkbrightly.com>
2013-03-30 14:15:36 -0400

    Renaming bootstrap to custom.
    This is to add some support for Prelude with my existing environment settings.

--- bootstrap.el
+++
@@ -1,6 +0,0 @@
-;; Use this file to load the environment. Sometimes scripts install garbage
-;; to the dotemacs file which is what init.el symbolizes.
-(load "external")
-(load "functions")
-(load "modes")
-(load "session")
---
+++ custom.el
@@ -0,0 +1,13 @@
+;;; package --- Use this file to load the environment. Sometimes scripts install garbage
+;;; to the dotemacs file which is what init.el symbolizes.
+
+;;; Code:
+(load "external")
+(load "functions")
+(load "keybindings")
+(load "modes")
+(load "session")
+
+(provide 'custom)
+
+;;; custom.el ends here
