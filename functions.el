d0e19371f17e896983a302585a618def69c86705
John Bellone <jb@thunkbrightly.com>
2013-03-30 14:15:58 -0400

    Adding some functions and global keybindings.
    Some things that I found useful from the new Emacs blog.

--- functions.el
+++ functions.el
@@ -3,3 +3,22 @@
   (interactive "*")
   (newline)
   (indent-relative))
+
+; http://emacsredux.com/blog/2013/03/30/kill-other-buffers/
+(defun kill-other-buffers ()
+  "Kill all buffers but the current one. Don't mess with special buffers."
+  (interactive)
+  (dolist (buffer (buffer-list))
+    (unless (or (eql buffer (current-buffer)) (not (buffer-file-name buffer)))
+      (kill-buffer buffer))))
+
+; http://emacsredux.com/blog/2013/03/29/terminal-at-your-fingertips/
+(defun visit-term-buffer ()
+  "Create or visit a terminal buffer."
+  (interactive)
+  (if (not (get-buffer "*ansi-term*"))
+      (progn
+        (split-window-sensibly (selected-window))
+        (other-window 1)
+        (ansi-term (getenv "SHELL")))
+    (switch-to-buffer-other-window "*ansi-term*")))
---
+++ keybindings.el
@@ -0,0 +1,4 @@
+(global-set-key (kbd "C-c k") 'kill-other-buffers)
+(global-set-key (kbd "C-c t") 'visit-term-buffer)
+(global-set-key (kbd "RET") 'newline-and-indent)
+
